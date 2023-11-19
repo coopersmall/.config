60f0bd34f6e5a217d9c11e51a4c9b94ad158dde7
coopersmall <cooper.small@gladly.com>
2023-11-19 10:23:00 -0700

    cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -36,6 +36,7 @@ echo "Installing packages..."
 echo
 
 install_pkg() {
+    echo "Checking if $1 is installed..."
     if ! $LIST_COMMAND $1 &> /dev/null
     then
         echo "Installing $1..."
@@ -46,71 +47,30 @@ install_pkg() {
     fi
 }
 
-echo "Checking if python is installed..."
 install_pkg python3
-
-echo "Checking if node is installed..."
 install_pkg node
-
-echo "Checking if npm is installed..."
 install_pkg npm
-
-echo "Checking if yarn is installed..."
 install_pkg yarn
-
-echo "Checking if go is installed..."
 install_pkg go
-
-echo "Checking if rust is installed..."
 install_pkg rust
-
-echo "Checking if docker is installed..."
 install_pkg docker
-
-echo "Checking if docker-compose is installed..."
 install_pkg docker-compose
-
-echo "Checking if kubectl is installed..."
 install_pkg kubectl
-
-echo "Checking if git is installed..."
 install_pkg git
-
-echo "Checking if curl is installed..."
 install_pkg curl
-
-echo "Checking if bat is installed..."
 install_pkg bat
-
-echo "Checking if exa is installed..."
 install_pkg exa
-
-echo "Checking if fd is installed..."
 install_pkg fd
-
-echo "Checking if fzf is installed..."
 install_pkg fzf
-
-echo "Checking if procs is installed..."
 install_pkg procs
-
-echo "Checking if ripgrep is installed..."
 install_pkg ripgrep
-
-echo "Checking if tldr is installed..."
 install_pkg tldr
-
-echo "Checking if htop is installed..."
 install_pkg htop
-
-echo "Checking if neovim is installed..."
 install_pkg neovim
-
-echo "Checking if zellij is installed..."
 install_pkg zellij
-
-echo "Checking if alacritty is installed..."
 install_pkg --cask alacritty
+install_pkg zsh
+install_pkg zsh-syntax-highlighting
 
 echo
 echo "Setting up directories..."
@@ -139,12 +99,6 @@ echo
 
 ZSHRC_PATH="$HOME/.zshrc"
 SCRIPT_ZSHRC=".zshrc"
-
-echo "Checking if zsh is installed..."
-install_pkg zsh
-
-echo "Checking if zsh-syntax-highlighting is installed..."
-install_pkg zsh-syntax-highlighting
 
 if [[ $SHELL != "/bin/zsh" ]]; then
     echo "Setting zsh as default shell..."
